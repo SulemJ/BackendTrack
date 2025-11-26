8daf43f6679c78dcc9408a5df05abcde015ed20d
SulemJ <sulemjibrildawud@gmail.com>
2025-11-26 23:45:02 +0300

    remove local tasks

--- task-6/task_manager/models/task.go
+++ task-6/task_manager/models/task.go
@@ -6,9 +6,3 @@ type Task struct {
 	Description string `json:"description"`
 	Deadline    string `json:"deadline"`
 }
-
-var Tasks = []Task{
-	{ID: "1", Title: "Eat dinner", Description: "nothing really", Deadline: "Nov 30"},
-	{ID: "2", Title: "sleep well", Description: "at 5 pm", Deadline: "always"},
-	{ID: "3", Title: "journal", Description: "always", Deadline: ""},
-}
